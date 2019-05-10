80a67672b704f31144dde0758f09463a182e2706
cantekfe <cantek.frontend@gmail.com>
2019-05-10 17:24:25 +0300

    package stabil

--- projects/apps/test-app/src/app/app.module.ts
+++ projects/apps/test-app/src/app/app.module.ts
@@ -5,13 +5,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 console.log("app comp", AppComponent);
 import { HelloComponent } from './hello.component';
-import { BarChartModule } from '../../../../c-energy-bar-chart/src/public_api';
+import { ListModule } from '../../../../c-dealers-company-list/src/public_api';
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    BarChartModule
+    ListModule
     
     ],
 
