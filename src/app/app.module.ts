988566cfaee6469f4d3880221b75c4bcaa82ad86
cantekfe <cantek.frontend@gmail.com>
2019-05-15 15:08:59 +0300

    folder revize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CompanyListComponent } from './company-list/company-list.component';
-import { CompanyDetailsComponent } from './company-details/company-details.component';
 import { DemoMaterialModule } from './demo-module/demo-material.module';
-import { CompanyService } from './services/company.service';
 import { HttpClientModule } from '@angular/common/http'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import{DealersListModule} from '../../projects/c-dealers-company-list/src/lib/list.module'
 @NgModule({
-  imports: [BrowserModule, FormsModule, DemoMaterialModule,HttpClientModule,BrowserAnimationsModule],
+  imports: [BrowserModule, FormsModule, DemoMaterialModule,HttpClientModule,BrowserAnimationsModule,DealersListModule],
   declarations: [
-    AppComponent,
-    CompanyListComponent,
-    CompanyDetailsComponent],
+    AppComponent,],
   bootstrap: [AppComponent],
-  providers: [CompanyService]
 })
 export class AppModule { }
